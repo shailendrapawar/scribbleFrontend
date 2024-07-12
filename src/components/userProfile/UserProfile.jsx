cfe7c47ef1dc25bf964da815721c67d861f126f3
shailendrapawar <shailendrapawar980@gmail.com>
2024-07-13 00:29:50 +0530

    tailwind added in user profile

--- src/components/userProfile/UserProfile.jsx
+++ src/components/userProfile/UserProfile.jsx
@@ -1,22 +1,33 @@
 import React from 'react'
 import './userProfile.css'
+import { useNavigate } from 'react-router-dom'
 const UserProfile = () => {
+
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+
+  }
+
+
+
   return (
-    <div className='userProfile-body'>
+    <div className='userProfile-body flex flex-col items-center bg-[#D9D9D9]'>
       <h1 className=' text-black text-center '>Account Info</h1>
 
-      <main className='userData '>
-        <section className='userData-left'>
+      <main className='userData flex '>
 
-          <div className='todo-count rounded-md relative flex'>
+        <section className='userData-left cursor-pointer flex flex-col gap-5 items-center justify-center'>
+          <div onClick={()=>navigate("/todo")} className='todo-count rounded-md relative flex items-center justify-center bg-[#0370FF]'>
             <b className='absolute top-2 left-2'>TODO:</b>   <span className=''>40</span>
           </div>
-          <div className='note-count rounded-md relative flex'>
+          <div  onClick={()=>navigate("/note")}  className='note-count rounded-md relative flex justify-center items-center bg-[#0370FF]'>
             <b className='absolute top-2 left-2'>NOTE:</b>   <span>50</span>
           </div>
-
         </section>
-        <section className='userData-right'>
+
+
+        <section className='userData-right flex flex-col justify-center items-center gap-5'>
           <div className='userName-body'>
             <span>name:</span><p>user name</p>
           </div>
@@ -29,7 +40,7 @@ const UserProfile = () => {
         </section>
       </main>
 
-      <button className=' bg-red-600 w-40 h-10 rounded-md' >LOG-OUT</button>
+      <button onClick={() => handleLogout()} className=' bg-red-500 w-40 h-10 rounded-md' >LOG-OUT</button>
 
     </div>
   );
